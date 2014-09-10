8d3a70934c80fe38fa6fc7219610e9d0d27a1fd8
n-zeplo <nzeplowitz@gmail.com>
2014-09-10 15:55:54 -0400

    Redirect From oAuth to Profile Page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -19,5 +19,5 @@ get '/callback' do
   puts '*' * 40
   p user_info = JSON.parse(response.body)
   #=> user_info is {"id"=>"108553686725590595849", "email"=>"nzeplowitz@gmail.com", "verified_email"=>true, "name"=>"Nathan Zeplowitz", "given_name"=>"Nathan", "family_name"=>"Zeplowitz", "link"=>"https://plus.google.com/108553686725590595849", "picture"=>"https://lh6.googleusercontent.com/-ljNCYRCxZ0g/AAAAAAAAAAI/AAAAAAAAAuk/r6hMRgwCzD0/photo.jpg", "gender"=>"male"}
-  puts "END"
+  redirect "/#{session[:current_user]}"
 end
