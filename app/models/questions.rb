0717210fc735385b0f023cad84d5edd1ba4ea620
n-zeplo <nzeplowitz@gmail.com>
2014-09-10 23:37:26 -0400

    Set Up Question Model

--- app/models/questions.rb
+++ app/models/questions.rb
@@ -1,3 +1,9 @@
-class Questions < ActiveRecord::Base
-  # Remember to create a migration!
+class Question < ActiveRecord::Base
+  after_initialize :defaults
+  belongs_to :user
+  has_many :active_questions
+
+  def defaults
+    self.status = 'unanswered'
+  end
 end
