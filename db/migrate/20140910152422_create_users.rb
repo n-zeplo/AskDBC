85a6ba187e80c7d66fab05c3b76254e2ccba012a
n-zeplo <nzeplowitz@gmail.com>
2014-09-10 23:39:31 -0400

    Change User Migration to Allow Different Access

--- db/migrate/20140910152422_create_users.rb
+++ db/migrate/20140910152422_create_users.rb
@@ -1,4 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string :email, null: false
+      t.string :name, null: false
+      t.string :picture
+      t.string :access
+
+      t.timestamps
+    end
   end
 end
