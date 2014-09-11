df30dc71b3d58590d65cc462ca450e2d27de9edd
n-zeplo <nzeplowitz@gmail.com>
2014-09-11 12:00:44 -0400

    Set Up New Event Handlers To Watch Button Clicks

---
+++ public/js/controller.js
@@ -0,0 +1,66 @@
+function Controller (view){
+  var user_questions = [];
+  this.view = view;
+
+  $.ajax({
+    url: '/questions/index',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(data){
+    user_questions = data['questions'];
+    view.updateQuestions(user_questions);
+  });
+
+  $(".button").click(function(e){
+    e.preventDefault();
+    $('#question_form').show();
+    $('.block_screen').show();
+  });
+
+  $('.block_screen').click(function(e){
+    e.preventDefault();
+    view.clearModal();
+  });
+
+  $('button').click(function(e){
+    e.preventDefault();
+    var question = view.getQuestion($("form.edit_question input[name='question[question]']").val(), user_questions);
+    if ($(this).attr('name') === 'delete'){
+      view.deleteQuestion(question);
+    } else if (($(this).attr('name') === 'push_edit')){
+      view.editQuestion(question);
+    } else if (($(this).attr('name') === 'activate')){
+
+    }
+  });
+
+  $('form.post_question').submit( function(e){
+    e.preventDefault();
+    view.postQuestion(this, user_questions);
+  });
+
+  $('.container').on('click', '.edit', function(e){
+    e.preventDefault();
+    var question = view.getQuestion($(this).text(), user_questions);
+    var attribute = $(this).closest('div').parent().closest('div').attr('id');
+    if (attribute === 'coach') {
+      view.showEditQuestion(question, 'answer');
+    } else {
+      view.showEditQuestion(question, 'edit');
+    }
+  });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
