25025b693b4f81a381f8dce62df1dc6a276d2997
n-zeplo <nzeplowitz@gmail.com>
2014-09-10 23:36:52 -0400

    Delete Tests and Fix Up Login Routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -15,9 +15,13 @@ get '/callback' do
 
   access_token = client.auth_code.get_token(params[:code], :redirect_uri => 'http://localhost:9393/callback')
   response = access_token.get('https://www.googleapis.com/oauth2/v1/userinfo?alt=json')
-  puts "START"
-  puts '*' * 40
-  p user_info = JSON.parse(response.body)
-  #=> user_info is {"id"=>"108553686725590595849", "email"=>"nzeplowitz@gmail.com", "verified_email"=>true, "name"=>"Nathan Zeplowitz", "given_name"=>"Nathan", "family_name"=>"Zeplowitz", "link"=>"https://plus.google.com/108553686725590595849", "picture"=>"https://lh6.googleusercontent.com/-ljNCYRCxZ0g/AAAAAAAAAAI/AAAAAAAAAuk/r6hMRgwCzD0/photo.jpg", "gender"=>"male"}
-  redirect "/#{session[:current_user]}"
+
+  user_info = JSON.parse(response.body)
+  p response.inspect
+  puts access_token.inspect
+  puts user_info
+  @user = User.find_or_create_by(email: user_info["email"], name: user_info["name"], picture: user_info["picture"]  )
+  session[:current_user] = @user.id
+
+  redirect "/users/#{session[:current_user]}"
 end
