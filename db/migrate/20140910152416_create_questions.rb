e50b67cdc8e467e9b50d99960071c2b6ed65ad55
n-zeplo <nzeplowitz@gmail.com>
2014-09-10 23:39:14 -0400

    Alter Question Migration to Have Description

--- db/migrate/20140910152416_create_questions.rb
+++ db/migrate/20140910152416_create_questions.rb
@@ -1,4 +1,17 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
+    create_table :questions do |t|
+      t.string :question, null: false
+      t.string :description
+      t.string :challenge_name
+      t.string :error_msg
+      t.string :location
+      t.string :code
+      t.string :status
+      t.string :answer
+      t.references :user
+
+      t.timestamps
+    end
   end
 end
